18f8a9a6b7c86d2976a6354d8960acc637a11f13
RaviduHM99 <raviduhm@gmail.com>
2025-06-20 10:31:26 +0530

    write_db check_drc issue

--- tcl/asic/scripts/innovus.tcl
+++ tcl/asic/scripts/innovus.tcl
@@ -181,9 +181,9 @@ write_def -floorplan -no_std_cells "$design(floorplan_def)"
 gui_fit
 
 # Reporting & Save
+write_db -common $design(dbs_dir)/pnr/floorplan.stylus.enc
 check_connectivity -type special > $design(pnr_reports)/2_floorplan/power_connectivity.rpt
 check_drc > $design(pnr_reports)/2_floorplan/drc_report.rpt
-write_db -common $design(dbs_dir)/pnr/floorplan.stylus.enc
 
 ####################################################
 # Placement
@@ -205,9 +205,9 @@ add_tieoffs -lib_cell "$tech(TIE_HIGH_CELL) $tech(TIE_LOW_CELL)" -prefix $tech(T
 opt_design -pre_cts -drv 
 
 # Reporting & Save
+write_db -common $design(dbs_dir)/pnr/placement.stylus.enc
 check_place > $design(pnr_reports)/3_placement/power_connectivity.rpt
 check_drc > $design(pnr_reports)/3_placement/drc_report.rpt
-write_db -common $design(dbs_dir)/pnr/placement.stylus.enc
 
 ####################################################
 # Clock Tree Synthesis
@@ -230,9 +230,9 @@ set_db opt_new_inst_prefix "cts_opt_inst_"
 set_db opt_new_net_prefix  "cts_opt_net_"
 ccopt_design -report_dir "$design(reports_dir)/pnr/4_clock_tree_synthesis/ccopt_design"
 
+write_db -common $design(dbs_dir)/pnr/pre_cts.stylus.enc
 check_connectivity > $design(pnr_reports)/5_post_cts_hold/cts_connectivity.rpt
 check_drc > $design(pnr_reports)/5_post_cts_hold/drc_report.rpt
-write_db -common $design(dbs_dir)/pnr/pre_cts.stylus.enc
 uom_report_timing $design(pnr_reports)/5_post_cts_hold
 
 # Open the clock tree debugger and check Clock Tree
@@ -244,9 +244,9 @@ uom_start_stage "5_post_cts_hold"
 opt_design -post_cts -hold 
 
 # Reporting & Save
+write_db -common $design(dbs_dir)/pnr/post_cts.stylus.enc
 check_connectivity > $design(pnr_reports)/5_post_cts_hold/cts_connectivity.rpt
 check_drc > $design(pnr_reports)/5_post_cts_hold/drc_report.rpt
-write_db -common $design(dbs_dir)/pnr/post_cts.stylus.enc
 uom_report_timing $design(pnr_reports)/5_post_cts_hold
 
 ####################################################
@@ -268,9 +268,9 @@ set_db opt_new_net_prefix "route_opt_net_"
 route_opt_design
 
 # Reporting & Save
+write_db -common $design(dbs_dir)/pnr/pre_route.stylus.enc
 check_connectivity > $design(pnr_reports)/6_pre_route/cts_connectivity.rpt
 check_drc > $design(pnr_reports)/6_pre_route/drc_report.rpt
-write_db -common $design(dbs_dir)/pnr/pre_route.stylus.enc
 uom_report_timing $design(pnr_reports)/6_pre_route
 
 # Post Route Optimization
@@ -292,9 +292,9 @@ add_fillers -cell $tech(FILL_CELL) -prefix $tech(FILL_CELL_PREFIX);
 route_eco -fix_drc
 
 # Reporting & Save
+write_db -common $design(dbs_dir)/pnr/post_route.stylus.enc
 check_connectivity > $design(pnr_reports)/7_post_route_opt/cts_connectivity.rpt
 check_drc > $design(pnr_reports)/7_post_route_opt/drc_report.rpt
-write_db -common $design(dbs_dir)/pnr/post_route.stylus.enc
 uom_report_timing $design(pnr_reports)/7_post_route_opt
 
 ####################################################
