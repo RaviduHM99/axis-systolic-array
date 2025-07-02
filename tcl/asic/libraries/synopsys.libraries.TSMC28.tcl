4f0d0005d9abda942bb23d49fb08e40f416eb5d7
RaviduHM99 <raviduhm@gmail.com>
2025-07-02 17:07:48 +0530

    synopsys initial library changes completed

--- tcl/asic/libraries/synopsys.libraries.TSMC28.tcl
+++ tcl/asic/libraries/synopsys.libraries.TSMC28.tcl
@@ -1,23 +1,23 @@
 # Route Technology settings for the TSMC 28 HPCPLUS Technology
 set TECHNOLOGY_NODE TSMC28HPCPLUS
-set METAL_STACK  1p8m_5x2z_utalrdl
-set METAL_LAYERS 9
-set TECH_NODE    28
+set METAL_STACK     1p8m_5x2z_utalrdl
+set METAL_LAYERS    9
+set TECH_NODE       28
 
 # Technology
-set HOME "../../.."
-set paths(PDK_ROOT) "$HOME/PDK/$TECHNOLOGY_NODE"
-set paths(TECHNOLOGY_FILES) "$paths(PDK_ROOT)/TECH_Libs"
-set paths(STANDARD_CELLS_TECH_FILES) "$paths(PDK_ROOT)/STD_Libs/lef"
+set HOME                                 "../../.."
+set paths(PDK_ROOT)                      "$HOME/PDK/$TECHNOLOGY_NODE"
+set paths(TECHNOLOGY_FILES)              "$paths(PDK_ROOT)/TECH_Libs"
+set paths(STANDARD_CELLS_MILKYWAY_FILES) "$paths(PDK_ROOT)/STD_Libs/milkyway"
+set paths(STANDARD_CELLS_TECH_FILES)     "$paths(PDK_ROOT)/STD_Libs/lef"
 
 # LEFS
-lappend tech(LEF_SUPPRESS_MESSAGES_GENUS) {*}"PHYS-279"
-lappend tech(LEF_SUPPRESS_MESSAGES_INNOVUS) {*}"IMPLF_20"
+# lappend tech(LEF_SUPPRESS_MESSAGES_GENUS) {*}"PHYS-279"
+# lappend tech(LEF_SUPPRESS_MESSAGES_INNOVUS) {*}"IMPLF_20"
 
-set tech_files(TECHNOLOGY_LEF) "$paths(TECHNOLOGY_FILES)/tech_lef/$METAL_STACK/sc9mcpp140z_tech.lef"
-    set tech_files(ALL_LEFS) [list $tech_files(TECHNOLOGY_LEF)]
-set tech_files(STANDARD_CELLS_LEF) "$paths(STANDARD_CELLS_TECH_FILES)/sc9mcpp140z_cln28ht_base_ulvt_c35.lef"
-    lappend tech_files(ALL_LEFS) $tech_files(STANDARD_CELLS_LEF)
+set tech_files(MILKYWAY_TF)      "$paths(TECHNOLOGY_FILES)/milkyway/$METAL_STACK/sc9mcpp140z_tech.tf"
+set tech_files(MILKYWAY_DATBASE) "$paths(STANDARD_CELLS_MILKYWAY_FILES)"
+set tech_files(STD_LEF)          "$paths(STANDARD_CELLS_TECH_FILES)/sc9mcpp140z_cln28ht_base_ulvt_c35.lef"
 
 # Temperatures for Corners
 set tech(TEMPERATURE_BC) -40
@@ -25,6 +25,7 @@ set tech(TEMPERATURE_TC) 85
 set tech(TEMPERATURE_WC) 125
 
 # Parasitic Extraction
-set tech_files(CAPTABLE_BC) "$paths(TECHNOLOGY_FILES)/captbl/$METAL_STACK/rcbest.captbl"
-set tech_files(CAPTABLE_TC) "$paths(TECHNOLOGY_FILES)/captbl/$METAL_STACK/typical.captbl"
-set tech_files(CAPTABLE_WC) "$paths(TECHNOLOGY_FILES)/captbl/$METAL_STACK/rcworst.captbl"
+set tech_files(TLUPLUS_BC)  "$paths(TECHNOLOGY_FILES)/synopsys_tluplus/$METAL_STACK/rcbest.tluplus"
+set tech_files(TLUPLUS_TC)  "$paths(TECHNOLOGY_FILES)/synopsys_tluplus/$METAL_STACK/typical.tluplus"
+set tech_files(TLUPLUS_WC)  "$paths(TECHNOLOGY_FILES)/synopsys_tluplus/$METAL_STACK/rcworst.tluplus"
+set tech_files(TLUPLUS_MAP) "$paths(TECHNOLOGY_FILES)/synopsys_tluplus/$METAL_STACK/tluplus.map"
